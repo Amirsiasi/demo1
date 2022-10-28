d3d655e2c6a88158ec7604d1ffc105be9abf250a
Amir <sia22813@spengergasse.at>
2022-10-28 04:06:36 +0200

    added enum orientaion

--- demo1-7acif/demo1-7acif/src/main/java/at/spengergasse/demo17acif/domain/Photo.java
+++ demo1-7acif/demo1-7acif/src/main/java/at/spengergasse/demo17acif/domain/Photo.java
@@ -1,4 +1,12 @@
 package at.spengergasse.demo17acif.domain;
 
+import java.time.LocalDate;
+
 public class Photo {
+    private String filename;
+    private String name;
+    private LocalDate creationTs;
+    private Integer width;
+    private Integer height;
+    private Oriention oriention;
 }
