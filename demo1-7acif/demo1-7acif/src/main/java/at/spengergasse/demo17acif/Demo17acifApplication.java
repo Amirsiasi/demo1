784d78cff5776cfe9c88aec224779720c2d8a36e
Amir <sia22813@spengergasse.at>
2022-10-28 03:45:47 +0200

    Add spring boot

---
+++ demo1-7acif/demo1-7acif/src/main/java/at/spengergasse/demo17acif/Demo17acifApplication.java
@@ -0,0 +1,13 @@
+package at.spengergasse.demo17acif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo17acifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo17acifApplication.class, args);
+	}
+
+}
